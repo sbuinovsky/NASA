1bc29774f7d68865420e7bbd085257e622b1d169
Stanislav Buinovsky <sbuinovsky@gmail.com>
2021-11-11 13:15:47 +0300

    Start

---
+++ InfoNASA/InfoNASA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoNASA
+//
+//  Created by Станислав Буйновский on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
