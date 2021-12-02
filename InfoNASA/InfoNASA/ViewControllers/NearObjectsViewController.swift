63ec69a1f3b22c1bad2737191000d1264d0d556e
Stanislav Buinovsky <sbuinovsky@gmail.com>
2021-12-02 15:16:28 +0300

    Add slider for days in NearEarthObjects controller Automatic filter data by slider If selected days less then array got from NetMan, no request to network and filtering array only

--- InfoNASA/InfoNASA/ViewControllers/NearObjectsViewController.swift
+++ InfoNASA/InfoNASA/ViewControllers/NearObjectsViewController.swift
@@ -11,35 +11,32 @@ class NearObjectsViewController: UIViewController {
 
     var tableView = UITableView()
     var activityIndicator = UIActivityIndicatorView()
+    var sliderView = UIView()
+    var sliderLabel = UILabel()
+    var slider = UISlider()
     
     private var objects: [String: [NearEarthObject]] = [:]
     private var objectsKeys: [String] = []
+    private var tempObjectsKeys: [String] = []
+    private var days: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        view.addSubview(tableView)
-        tableView.addSubview(activityIndicator)
+        view.addSubview(sliderView)
+        sliderView.addSubview(sliderLabel)
+        sliderView.addSubview(slider)
+        configureSlider()
+        configureSliderLabel()
         
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
         configureTableView()
         configureActivityIndicator()
         setConstraints()
         
-        let dateInterval = getDateInterval(for: 2)
-        
-        NetworkManager.shared.fetchNearEarthObjects(forDateInterval: dateInterval) { [weak self] result in
-            switch result {
-            case .success(let nearEarthObjectsDict):
-                guard let nearEarthObjectsDict = nearEarthObjectsDict else { return }
-                self?.objects = nearEarthObjectsDict
-                self?.objectsKeys = Array((self?.objects.keys)!).sorted(by: >)
-                self?.activityIndicator.stopAnimating()
-                self?.tableView.reloadData()
-            case .failure(_):
-                print("failure")
-            }
-        }
+        updateTableView(for: days)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -48,25 +45,80 @@ class NearObjectsViewController: UIViewController {
         tabBarController?.title = "Near Earth objects"
     }
     
+    private func configureSliderLabel() {
+        sliderLabel.text = "Days: \(days)"
+        sliderLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+    }
+    
+    private func configureSlider() {
+        slider.minimumValue = 1
+        slider.maximumValue = 5
+        slider.value = Float(days)
+        slider.addTarget(self, action: #selector(sliderEndChanging), for: .touchUpInside)
+        slider.addTarget(self, action: #selector(sliderChanging), for: .valueChanged)
+    }
+    
+    private func updateSlider() {
+        slider.value = Float(days)
+    }
+    
+    @objc
+    private func sliderEndChanging(sender: UISlider) {
+        activityIndicator.startAnimating()
+        tableView.layer.opacity = 0.2
+        configureSliderLabel()
+        updateSlider()
+        updateTableView(for: days)
+    }
+    
+    @objc
+    private func sliderChanging(sender: UISlider) {
+        days = Int(sender.value)
+        configureSliderLabel()
+    }
+    
     private func configureTableView() {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(NearObjectsCell.self, forCellReuseIdentifier: "nearObjectsCell")
-        tableView.sectionHeaderHeight = 60
+        tableView.sectionHeaderHeight = 40
     }
     
     private func configureActivityIndicator() {
         activityIndicator.startAnimating()
         activityIndicator.hidesWhenStopped = true
         activityIndicator.style = .large
+        tableView.layer.opacity = 0.2
     }
     
     private func setConstraints() {
+        sliderView.translatesAutoresizingMaskIntoConstraints = false
+        sliderLabel.translatesAutoresizingMaskIntoConstraints = false
+        slider.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         
+        let sliderViewConstraints = [
+            sliderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            sliderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            sliderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ]
+        
+        let sliderLabelConstraints = [
+            sliderLabel.topAnchor.constraint(equalTo: sliderView.topAnchor, constant: 10),
+            sliderLabel.leadingAnchor.constraint(equalTo: sliderView.leadingAnchor, constant: 20),
+            sliderLabel.trailingAnchor.constraint(equalTo: sliderView.trailingAnchor, constant: -20)
+        ]
+        
+        let sliderConstraints = [
+            slider.topAnchor.constraint(equalTo: sliderLabel.bottomAnchor, constant: 10),
+            slider.centerXAnchor.constraint(equalTo: sliderLabel.centerXAnchor),
+            slider.widthAnchor.constraint(equalTo: sliderLabel.widthAnchor),
+            slider.bottomAnchor.constraint(equalTo: sliderView.bottomAnchor, constant: -10)
+        ]
+        
         let tableViewConstraints = [
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.topAnchor.constraint(equalTo: sliderView.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
@@ -77,26 +129,56 @@ class NearObjectsViewController: UIViewController {
             activityIndicator.centerYAnchor.constraint(equalTo: tableView.centerYAnchor)
         ]
         
+        NSLayoutConstraint.activate(sliderViewConstraints)
+        NSLayoutConstraint.activate(sliderLabelConstraints)
+        NSLayoutConstraint.activate(sliderConstraints)
         NSLayoutConstraint.activate(tableViewConstraints)
         NSLayoutConstraint.activate(activityIndicatorConstraints)
     }
     
     private func getDateInterval(for days: Int) -> [String: String] {
         let startDate = Date()
-        let timeInterval = TimeInterval(-3600 * 24 * days)
+        let timeInterval = TimeInterval(-3600 * 24 * (days - 1))
         let endDate = Date(timeInterval: timeInterval, since: startDate)
         return NetworkManager.shared.getDateInterval(from: startDate, to: endDate)
+    }
+    
+    private func updateTableView(for days: Int) {
+        
+        if objects.count >= days {
+            tempObjectsKeys = Array(objectsKeys.prefix(days))
+            activityIndicator.stopAnimating()
+            tableView.layer.opacity = 1
+            tableView.reloadData()
+            
+        } else {
+            let dateInterval = getDateInterval(for: days)
+            NetworkManager.shared.fetchNearEarthObjects(forDateInterval: dateInterval) { [weak self] result in
+                switch result {
+                case .success(let nearEarthObjectsDict):
+                    guard let nearEarthObjectsDict = nearEarthObjectsDict else { return }
+                    self?.objects = nearEarthObjectsDict
+                    self?.objectsKeys = Array((self?.objects.keys)!).sorted(by: >)
+                    self?.tempObjectsKeys = self?.objectsKeys ?? []
+                    self?.activityIndicator.stopAnimating()
+                    self?.tableView.layer.opacity = 1
+                    self?.tableView.reloadData()
+                case .failure(_):
+                    print("failure")
+                }
+            }
+        }
     }
 }
 
 extension NearObjectsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        objectsKeys.count
+        tempObjectsKeys.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let key = objectsKeys[section]
+        let key = tempObjectsKeys[section]
         return objects[key]?.count ?? 0
     }
 
@@ -105,7 +187,7 @@ extension NearObjectsViewController: UITableViewDataSource, UITableViewDelegate 
 
         tableView.deselectRow(at: indexPath, animated: true)
 
-        let key = objectsKeys[indexPath.section]
+        let key = tempObjectsKeys[indexPath.section]
         let object = objects[key]?[indexPath.row]
         if let object = object {
             cell.configure(with: object)
@@ -115,7 +197,6 @@ extension NearObjectsViewController: UITableViewDataSource, UITableViewDelegate 
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        "Date: \(objectsKeys[section])"
+        "Date: \(tempObjectsKeys[section])"
     }
-
 }
